36a6a6253f4c3a882fe3eff573e1f86ac8698890
Mark Edward Ratio <38338316+MERatio@users.noreply.github.com>
2023-11-30 21:44:44 +0800

    Refactor JS

--- src/js/Tree.js
+++ src/js/Tree.js
@@ -7,6 +7,43 @@ class Tree {
     this.root = this.#buildTree(uniqueSortedArr);
   }
 
+  static prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false,
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
+  }
+
+  static getHeight(node) {
+    if (node === null) {
+      return 0;
+    }
+    return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
+  }
+
+  static isBalanced(node) {
+    if (node === null) {
+      return true;
+    }
+    const leftHeight = this.getHeight(node.left);
+    const rightHeight = this.getHeight(node.right);
+    return (
+      Math.abs(leftHeight - rightHeight) <= 1 &&
+      this.isBalanced(node.left) &&
+      this.isBalanced(node.right)
+    );
+  }
+
   #buildTree(arr) {
     if (arr.length === 0) {
       return null;
@@ -23,9 +60,9 @@ class Tree {
       node = new Node(data);
       return node;
     }
-    if (data < node.data) {
+    if (node.data > data) {
       node.left = this.#insertRec(node.left, data);
-    } else if (data > node.data) {
+    } else if (node.data < data) {
       node.right = this.#insertRec(node.right, data);
     } else {
       throw new Error("Trees cannot contain any duplicate values.");
@@ -39,10 +76,10 @@ class Tree {
       return node;
     }
     // Find the node to be deleted.
-    if (data < node.data) {
+    if (node.data > data) {
       node.left = this.#deleteRec(node.left, data);
       return node;
-    } else if (data > node.data) {
+    } else if (node.data < data) {
       node.right = this.#deleteRec(node.right, data);
       return node;
     } else {
@@ -88,43 +125,6 @@ class Tree {
     } else {
       return node;
     }
-  }
-
-  static prettyPrint(node, prefix = "", isLeft = true) {
-    if (node === null) {
-      return;
-    }
-    if (node.right !== null) {
-      this.prettyPrint(
-        node.right,
-        `${prefix}${isLeft ? "│   " : "    "}`,
-        false,
-      );
-    }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
-    if (node.left !== null) {
-      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
-    }
-  }
-
-  static getHeight(node) {
-    if (node === null) {
-      return 0;
-    }
-    return Math.max(this.getHeight(node.left), this.getHeight(node.right)) + 1;
-  }
-
-  static isBalanced(node) {
-    if (node === null) {
-      return true;
-    }
-    const leftHeight = this.getHeight(node.left);
-    const rightHeight = this.getHeight(node.right);
-    return (
-      Math.abs(leftHeight - rightHeight) <= 1 &&
-      this.isBalanced(node.left) &&
-      this.isBalanced(node.right)
-    );
   }
 
   insert(data) {
